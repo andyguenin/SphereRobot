b716f053ac14132017b8872287ef3c06a91682e2
Andy Guenin <andyguenin@gmail.com>
2012-02-17 16:06:54 -0500

    Added a JUnit test class for future unit testing

---
+++ com/spherebot/test/RunnerTest.java
@@ -0,0 +1,15 @@
+package com.spherebot.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class RunnerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
